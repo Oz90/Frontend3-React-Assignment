3871be363b2aaf1bc09d02c47a03db78d5163c16
Oz90 <gurr__@hotmail.com>
2021-01-10 16:23:37 +0100

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/customers">Customers</Link>
+        </li>
+        <li>
+          <Link to="/customers/create">Create Customer</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/customers/create"></Route>
+        <Route path="/customers/:id/edit"></Route>
+        <Route path="/customers/:id"></Route>
+        <Route path="/customers"></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
