cdd262afdde99aa3e26fc6fe3217cfec1240baf2
Oz90 <gurr__@hotmail.com>
2021-01-13 19:05:57 +0100

    Initializing usercontext

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import CustomerListPage from "./pages/CustomerListPage";
 import CustomerDetailPage from "./pages/CustomerDetailPage";
 import CustomerCreatePage from "./pages/CustomerCreatePage";
 import CustomerUpdatePage from "./pages/CustomerUpdatePage";
+import { UserContext } from "./context/UserContext";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/customers/create">
-          <CustomerCreatePage />
-        </Route>
-        <Route path="/customers/:id/edit" component={CustomerUpdatePage} />
-        <Route path="/customers/:id" component={CustomerDetailPage} />
-        <Route path="/customers">
-          <CustomerListPage />
-        </Route>
-      </Switch>
+      <UserContext.Provider>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/customers/create">
+            <CustomerCreatePage />
+          </Route>
+          <Route path="/customers/:id/edit" component={CustomerUpdatePage} />
+          <Route path="/customers/:id" component={CustomerDetailPage} />
+          <Route path="/customers">
+            <CustomerListPage />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </div>
   );
 }
